0dd4b4b55083365b7b7754786a5a80bf28c6a3ec
jonas mendes <jonas.jsm2903@gmail.com>
2025-02-05 12:58:27 -0300

    #1 iniciando na linguagem

---
+++ #1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Digite seu nome: ");
+String nome = Console.ReadLine();
+
+Console.WriteLine("qual o seu peso? ");
+Double peso = double.Parse(Console.ReadLine());
+
+Console.WriteLine("sua altura? ");
+double altura = double.Parse(Console.ReadLine());
+
+double imc = peso / (altura * altura);
+
+Console.WriteLine("seu nome é " + nome + " e pesa: " + peso + "kl  sua altura é de: " + altura);
+
+Console.WriteLine(nome + " seu Imc é: " + imc);
---
+++ #2/OlaSouCSharp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Digite seu nome: ");
+String nome = Console.ReadLine();
+
+
+Console.WriteLine("bem-vindo " + nome);
